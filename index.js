284af10e30e72134c236170c600d294b37253404
eunicepague <eunicegracepague@gmail.com>
2023-09-29 05:06:05 +0800

    added some categories

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 
@@ -10,28 +10,24 @@ const userRoute = require('./routes/userRoute');
 const cakeRoute = require('./routes/cakeRoute');
 
 // DB Connection
-mongoose.connect(process.env.MONGO_URL, 
-	{useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // DB Notif or Test Connection
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log(`Connected to Database`));
 
-
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // app.get("/", (req, res)=> res.send("hello"));
 
-
 //connect router module
-app.use("/api/user", userRoute);
-app.use("/api/cake", cakeRoute);
-
+app.use('/api/user', userRoute);
+app.use('/api/cake', cakeRoute);
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
